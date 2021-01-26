220c6601744d428c9ae77762fa3b6a6a7cf82608
Ibraheem-A <43247583+Ibraheem-A@users.noreply.github.com>
2021-01-27 00:33:59 +0100

    add search button

--- app/src/main/java/com/example/bluetoothfinder/MainActivity.kt
+++ app/src/main/java/com/example/bluetoothfinder/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.bluetoothfinder
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ListView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+
 
 class MainActivity : AppCompatActivity() {
+    var listView: ListView? = null
+    var statusTextView: TextView? = null
+    var searchButton: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        listView = findViewById(R.id.listView)
+        statusTextView = findViewById(R.id.statusTextView)
+        searchButton = findViewById(R.id.searchButton)
+    }
+
+    fun onSearchClicked(view: View) {
+        Log.i("Search Button Clicked", "Searching...")
+        statusTextView?.text = getString(R.string.searching)
+        searchButton?.isEnabled = false
+        searchButton?.isClickable = false
+        searchButton?.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.greyish))
     }
 }
